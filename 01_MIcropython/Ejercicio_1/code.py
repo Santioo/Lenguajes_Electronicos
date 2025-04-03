9dc76154119ea10f8bfcb7eefdb147188c716dc1
Santioo <Santiodev@gmail.com>
2025-04-03 08:34:03 -0300

    Update code.py
    Parametrizo la espera

--- 01_MIcropython/Ejercicio_1/code.py
+++ 01_MIcropython/Ejercicio_1/code.py
@@ -3,13 +3,15 @@ from utime import sleep
 
 print("Hello, ESP32!")
 
+espera = 1
+
 led = Pin(15, Pin.OUT)
 led2 = Pin(2, Pin.OUT)
 
 while True:
   led.on()
-  sleep(1)
+  sleep(espera)
   led.off()
   led2.on()
-  sleep(1)
+  sleep(espera)
   led2.off()
